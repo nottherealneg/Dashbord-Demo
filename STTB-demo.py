ef0830b6ca5c1838c184e26918ec1dc8801aa11e
Negin <111083117+nottherealneg@users.noreply.github.com>
2024-07-04 22:26:03 +0330

    .

--- STTB-demo.py
+++ STTB-demo.py
@@ -64,7 +64,7 @@ logo = Image.open('logo.png')
 col1, col2 = st.columns([1, 3]) 
 
 with col1:
-    st.image(logo, width=150)  
+    st.image(logo, width=200)  
 
 with col2:
     st.markdown('<p class="custom-title">مانیتورینگ هوشمند شرکت سـولار تابش توان بین الملل</p>', unsafe_allow_html=True)
