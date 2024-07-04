63ff29d3ff67532445ea7c4f2f5128ed5237d784
Negin <111083117+nottherealneg@users.noreply.github.com>
2024-07-04 22:27:59 +0330

    .

--- STTB-demo.py
+++ STTB-demo.py
@@ -4,6 +4,7 @@ import pandas as pd
 from PIL import Image
 import plotly.graph_objs as go
 import plotly.express as px
+import base64
 
 #PAGE CONFIG
 st.set_page_config(
@@ -26,6 +27,22 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # add background
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+add_bg_from_local('bg.jpg')
 ####
 
 #sidebar
