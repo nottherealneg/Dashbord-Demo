245f6b82c865bcc01f29fc1170a427d017f91ba9
Negin <111083117+nottherealneg@users.noreply.github.com>
2024-07-05 09:26:32 +0330

    .

--- STTB-demo.py
+++ STTB-demo.py
@@ -68,35 +68,8 @@ with st.sidebar:
 
 #################################### chatbot 
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
 
-# React to user input
-if prompt := st.chat_input("سلام خوش آمدید"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
- 
-    response = f"شما گفتید: {prompt}"
-
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
-
-# Add a button to clear the chat history
-if st.button("پاک کردن تاریخچه گفتگو"):
-    st.session_state.messages = []
-    st.experimental_rerun()
 
 #############################
 
