6bcabae828939ffad21fb6dcba5a4004a736ded2
Negin <111083117+nottherealneg@users.noreply.github.com>
2024-07-05 09:20:10 +0330

    .

--- STTB-demo.py
+++ STTB-demo.py
@@ -60,8 +60,7 @@ with st.sidebar:
     with st.echo():
         st.write("This code will be printed to the sidebar.")
 
-    with st.spinner("Loading..."):
-        time.sleep(5)
+    
     st.success("Done!")
     st.markdown(' خوش آمدید' )
     home = st.button("  🏠 خانه ")
